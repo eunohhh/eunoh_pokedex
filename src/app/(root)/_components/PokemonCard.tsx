c94bb3a91c12aced7f6fee394bcb6059ec57fb14
Eun Oh <bdohhhhh@gmail.com>
2024-07-01 22:51:26 +0900

    240701 [update]: card css update 2

--- src/app/(root)/_components/PokemonCard.tsx
+++ src/app/(root)/_components/PokemonCard.tsx
@@ -15,13 +15,11 @@ function PokemonCard({ pokemon }: PokemonCardProps) {
     const handleMouseMove = (
         e: React.MouseEvent<HTMLLIElement> | React.MouseEvent<HTMLDivElement>
     ) => {
-        console.log("mouse move");
-
         const x = e.nativeEvent.offsetX;
         const y = e.nativeEvent.offsetY;
         let rotateY = (-1 / 5) * x + 20;
         let rotateX = (4 / 30) * y - 20;
-        e.currentTarget.style.backgroundPosition = `${x / 5 + y / 5}%`;
+        e.currentTarget.style.backgroundPosition = `${x / 5 + y / 5}% ${x / 5 + y / 5}%`;
         e.currentTarget.style.filter = `opacity(${x / 200}) brightness(1.2)`;
         if (liRef.current)
             liRef.current.style.transform = `perspective(350px) rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
@@ -30,7 +28,7 @@ function PokemonCard({ pokemon }: PokemonCardProps) {
     const handleMouseLeave = (
         e: React.MouseEvent<HTMLLIElement> | React.MouseEvent<HTMLDivElement>
     ) => {
-        e.currentTarget.style.filter = "opacity(0)";
+        e.currentTarget.style.filter = "opacity(0.6)";
         if (liRef.current)
             liRef.current.style.transform = "perspective(350px) rotateY(0deg) rotateX(0deg)";
     };
@@ -39,17 +37,17 @@ function PokemonCard({ pokemon }: PokemonCardProps) {
     return (
         <Link
             href={`/pokemons/${pokemon.id}`}
-            className="relative flex justify-center items-center transition-all duration-100"
+            className="relative flex justify-center items-center "
         >
-            <div
-                onMouseMove={handleMouseMove}
-                onMouseLeave={handleMouseLeave}
-                className={`z-10 absolute w-[244px] h-96 filter-brightness-opacity transition-all duration-100 ${styles.bg_custom_gradient} ${styles.filter_brightness_opacity} ${styles.mix_blend_color_dodge} ${styles.bg_size} ${styles.bg_position}`}
-            />
             <li
                 ref={liRef}
-                className="h-96 w-80 flex flex-col items-center justify-center gap-4 rounded-lg bg-gray-200 shadow-md duration-400 ease-in-out transition transform hover:shadow-lg"
+                className="h-96 w-80 flex flex-col items-center justify-center gap-4 rounded-lg bg-gray-200 shadow-md duration-400 ease-in-out transition-all duration-100 transform hover:shadow-lg"
             >
+                <div
+                    onMouseMove={handleMouseMove}
+                    onMouseLeave={handleMouseLeave}
+                    className={`z-10 absolute w-[244px] h-96 filter-brightness-opacity transition-all mix-blend-color-dodge bg-[size:150%_150%] bg-[position:100%] brightness-110 ${styles.bg_custom_gradient} ${styles.filter_opacity}`}
+                />
                 <div className="relative aspect-auto w-52 h-52">
                     <Image
                         src={pokemon.sprites.other.dream_world.front_default as string}
